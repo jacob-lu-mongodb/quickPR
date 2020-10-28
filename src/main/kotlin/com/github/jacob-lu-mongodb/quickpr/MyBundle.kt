8ac93f9128b380e10a31136b54147551458b0185
GitHub Action <action@github.com>
2020-10-28 06:49:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/jacob-lu-mongodb/quickpr/MyBundle.kt
+++ src/main/kotlin/com/github/jacob-lu-mongodb/quickpr/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jacob-lu-mongodb.quickpr
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/jacob-lu-mongodb/quickpr/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jacob-lu-mongodb/quickpr/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jacob-lu-mongodb.quickpr.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jacob-lu-mongodb.quickpr.MyBundle
 
 class MyApplicationService {
 
