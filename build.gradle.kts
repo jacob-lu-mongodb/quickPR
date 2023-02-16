1c0a0e4c47d852760d1d7e7334755b67154d515a
Jacob Lu <jacob.lu@mongodb.com>
2023-02-15 23:56:47 -0600

    change kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,8 +75,8 @@ tasks {
         }
         withType<KotlinCompile> {
             kotlinOptions.jvmTarget = it
-            kotlinOptions.apiVersion = "1.7"
-            kotlinOptions.languageVersion = "1.7"
+            kotlinOptions.apiVersion = "1.8"
+            kotlinOptions.languageVersion = "1.8"
         }
     }
 
